7305e0a393942823a2e5c0d3e4d5ee7e503021ca
AbdalahBeshary <146075961+AbdalahBeshary@users.noreply.github.com>
2023-10-17 03:58:23 +0300

    Delete lab1_part3.m

--- lab1_part3.m
+++
@@ -1,5 +0,0 @@
-originalMatrix =randi(2,2);
-repeatedMatrix=vertcat(horzcat(originalMatrix,originalMatrix),horzcat(originalMatrix,originalMatrix));
-%other way
-repeatedMatrix=repmat(repeatedMatrix,1,1);
-disp(repeatedMatrix);
