bb5c70b2e7ef40a8631327ead6496616bd0948bc
AbdalahBeshary <146075961+AbdalahBeshary@users.noreply.github.com>
2023-12-26 22:21:48 +0200

    Add files via upload

---
+++ Curise_Control_system/Car_Modes.m
@@ -0,0 +1,13 @@
+classdef (Enumeration) Car_Modes< Simulink.IntEnumType
+
+        enumeration
+               Accelerate(0)
+               Decelerate(1)
+               Do_Nothing(2)
+               Stop(3)
+               brake(4)
+               Cruiser_Off(5)
+               Fault(6)
+        end
+
+ end
